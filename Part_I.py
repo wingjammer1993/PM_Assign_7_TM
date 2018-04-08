d50b6b15f6448247a72644ad467c400d05465f02
amruta <hail.radical@gmail.com>
2018-04-08 11:32:56 -0600

    ver_1

--- Part_I.py
+++ Part_I.py
@@ -18,20 +18,19 @@ def create_documents(i_alpha, i_beta):
 	for i in range(0, 200):
 		actual_doc = ''
 		topic_draw = np.random.dirichlet([i_alpha]*3)
-		samples_per_topic = [floor(x*50) for x in topic_draw]
-		if sum(samples_per_topic) != 50:
-			num = np.random.choice([0, 1, 2])
-			samples_per_topic[num] = samples_per_topic[num] + (50 - sum(samples_per_topic))
-			for k in range(0, abs(samples_per_topic[0])):
+		for j in range(0, 50):
+			topic_distribution = np.random.multinomial(50, topic_draw, size=1)
+			for p in range(0, abs(topic_distribution[0][0])):
 				topic_0 = np.argmax(np.random.dirichlet([i_beta]*7))
 				actual_doc = actual_doc + (chr(65 + int(topic_0)))  # A to G
-			for k in range(0, abs(samples_per_topic[1])):
+			for q in range(0, abs(topic_distribution[0][1])):
 				topic_1 = np.argmax(np.random.dirichlet([i_beta]*7))
 				actual_doc = actual_doc + (chr(72 + int(topic_1)))   # H to N
-			for k in range(0, abs(samples_per_topic[2])):
+			for r in range(0, abs(topic_distribution[0][2])):
 				topic_2 = np.argmax(np.random.dirichlet([i_beta]*6))
 				actual_doc = actual_doc + (chr(79 + int(topic_2)))   # O to T
 		documents.append(actual_doc)
+	print(documents)
 	return documents
 
 
@@ -40,7 +39,7 @@ def latent_dirichlet_allocation(documents, i_num_topics, i_alpha, i_beta):
 	tf = count_vec.fit_transform(documents)
 	tf_feature_names = count_vec.get_feature_names()
 	lda = LatentDirichletAllocation(n_topics=i_num_topics, doc_topic_prior=i_alpha, topic_word_prior=i_beta).fit(tf)
-	no_top_words = 5
+	no_top_words = 10
 	display_topics(lda, tf_feature_names, no_top_words)
 
 
