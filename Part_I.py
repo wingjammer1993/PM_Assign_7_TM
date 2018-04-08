8a31623a8f94b3b9b020f2c84abdce7702090e9d
amruta <hail.radical@gmail.com>
2018-04-08 12:10:08 -0600

    ver_1

--- Part_I.py
+++ Part_I.py
@@ -15,30 +15,31 @@ def display_topics(model, feature_names, no_top_words):
 
 def create_documents(i_alpha, i_beta):
 	documents = []
-	for i in range(0, 200):
+	topic_draw = np.random.dirichlet([i_alpha]*3, size=200)
+	for i in topic_draw:
 		actual_doc = ''
-		topic_draw = np.random.dirichlet([i_alpha]*3)
 		for j in range(0, 50):
-			topic_distribution = np.random.multinomial(50, topic_draw, size=1)
-			for p in range(0, abs(topic_distribution[0][0])):
+			topic_distribution = np.random.multinomial(1, i, size=1)
+			if topic_distribution[0][0] == 1:
 				topic_0 = np.argmax(np.random.dirichlet([i_beta]*7))
-				actual_doc = actual_doc + (chr(65 + int(topic_0)))  # A to G
-			for q in range(0, abs(topic_distribution[0][1])):
+				actual_doc = actual_doc + (chr(65 + int(topic_0))) + " "  # A to G
+			if topic_distribution[0][1] == 1:
 				topic_1 = np.argmax(np.random.dirichlet([i_beta]*7))
-				actual_doc = actual_doc + (chr(72 + int(topic_1)))   # H to N
-			for r in range(0, abs(topic_distribution[0][2])):
+				actual_doc = actual_doc + (chr(72 + int(topic_1))) + " "  # H to N
+			if topic_distribution[0][2] == 1:
 				topic_2 = np.argmax(np.random.dirichlet([i_beta]*6))
-				actual_doc = actual_doc + (chr(79 + int(topic_2)))   # O to T
+				actual_doc = actual_doc + (chr(79 + int(topic_2))) + " "  # O to T
 		documents.append(actual_doc)
 	print(documents)
 	return documents
 
 
 def latent_dirichlet_allocation(documents, i_num_topics, i_alpha, i_beta):
-	count_vec = CountVectorizer(vocabulary=words)
+	count_vec = CountVectorizer(stop_words=None, analyzer='char', lowercase = False)
 	tf = count_vec.fit_transform(documents)
+	k = tf.todense()
 	tf_feature_names = count_vec.get_feature_names()
-	lda = LatentDirichletAllocation(n_topics=i_num_topics, doc_topic_prior=i_alpha, topic_word_prior=i_beta).fit(tf)
+	lda = LatentDirichletAllocation(n_components=i_num_topics, doc_topic_prior=i_alpha, topic_word_prior=i_beta).fit(tf)
 	no_top_words = 10
 	display_topics(lda, tf_feature_names, no_top_words)
 
