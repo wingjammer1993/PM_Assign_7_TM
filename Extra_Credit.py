5f7b7b298c5e5e725284d49f8302d8c91af4848d
amruta <hail.radical@gmail.com>
2018-04-11 22:27:00 -0600

    Modified distributions, and plots

--- Extra_Credit.py
+++ Extra_Credit.py
@@ -1,8 +1,15 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import LatentDirichletAllocation
+import io
 
-dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+
+def big_doc_c(input_document):
+    ip_test = []
+    ip = io.open(input_document,'r', encoding='utf8')
+    for ip_line in ip.readlines():
+        result = ''.join([i for i in ip_line if not i.isdigit()])
+        ip_test.append(result)
+    return ip_test
 
 
 def display_topics(model, feature_names, top_words):
@@ -12,12 +19,12 @@ def display_topics(model, feature_names, top_words):
                         for i in topic.argsort()[:-top_words - 1:-1]]))
 
 
-documents = dataset.data
+text = big_doc_c('medical.txt')
 no_features = 1000
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
-tf = tf_vectorizer.fit_transform(documents)
+tf = tf_vectorizer.fit_transform(text)
 tf_feature_names = tf_vectorizer.get_feature_names()
-no_topics = 20
+no_topics = 10
 # Run LDA
 lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
 no_top_words = 10
